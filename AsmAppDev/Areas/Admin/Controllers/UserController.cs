4b626294c4f38f2ff68b8b29f8d9d963ccf0d417
twoB21 <nguyentruongv2@gmail.com>
2024-12-11 10:08:31 +0700

    hehe

--- AsmAppDev/Areas/Admin/Controllers/UserController.cs
+++ AsmAppDev/Areas/Admin/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace AsmAppDev.Areas.Admin.Controllers
 {
@@ -13,12 +14,15 @@ namespace AsmAppDev.Areas.Admin.Controllers
 	{
 		private readonly IUnitOfWork _unitOfWork;
 		private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-		public UsersController(IUnitOfWork unitOfWork, UserManager<IdentityUser> userManager)
+        public UsersController(IUnitOfWork unitOfWork, UserManager<IdentityUser> userManager,  RoleManager<IdentityRole> roleManager)
 		{
 			_unitOfWork = unitOfWork;
 			_userManager = userManager;
-		}
+            _roleManager = roleManager;
+
+        }
 
 		public IActionResult Index()
 		{
@@ -84,6 +88,56 @@ namespace AsmAppDev.Areas.Admin.Controllers
 			TempData["Success"] = "Account deleted successfully!";
 			return RedirectToAction(nameof(Index));
 		}
-	}
+
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var roles = await _roleManager.Roles.ToListAsync();
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var model = new EditRoleViewModel
+            {
+                UserId = user.Id,
+                UserName = user.UserName,
+                Roles = roles.Select(r => new RoleSelection
+                {
+                    RoleName = r.Name,
+                    IsSelected = userRoles.Contains(r.Name) 
+                }).ToList()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model, string Roles)
+        {
+            var user = await _userManager.FindByIdAsync(model.UserId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var currentRoles = await _userManager.GetRolesAsync(user);
+
+            var rolesToRemove = currentRoles.Where(r => r != Roles).ToList();
+            if (rolesToRemove.Any())
+            {
+                await _userManager.RemoveFromRolesAsync(user, rolesToRemove);
+            }
+            if (!currentRoles.Contains(Roles))
+            {
+                await _userManager.AddToRoleAsync(user, Roles);
+            }
+
+            TempData["Success"] = "Role updated successfully!";
+            return RedirectToAction("Index");
+        }
+
+    }
 
 }
---
+++ AsmAppDev/Models/EditRoleViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace AsmAppDev.Models
+{
+    public class EditRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public List<RoleSelection> Roles { get; set; }
+    }
+
+    public class RoleSelection
+    {
+        public string RoleName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+
+
+}
